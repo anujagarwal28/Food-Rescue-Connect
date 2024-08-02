6d24a3c056da45916b193f4eab7ee3d49d8328f7
Anuj Agarwal <anujkrishna2002@gmail.com>
2024-08-03 04:17:49 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,244 @@
+let mainpage1= document.querySelector(".hero");
+let mainpage2= document.querySelector(".cloth-wear");
+let about1 = document.querySelector(".about-msg1");
+let about2 = document.querySelector(".about-msg2");
+let our_partner = document.querySelector(".our-partners");
+let feedback_form = document.getElementById("feedback-form");
+let signup_form = document.getElementById("signup-form");
+let signin_form = document.getElementById("signin-form");
+let food_donation_form = document.getElementById("food-donation-form");
+let food_require_form = document.getElementById("food-require-form");
+let foot = document.querySelector(".footer");
+
+function main(){
+    
+    foot.style.display="block";
+    mainpage1.style.display = "block";
+    mainpage2.style.display = "block";
+    about1.style.display = "none";
+    about2.style.display = "none";
+    feedback_form.style.display= "none";
+    our_partner.style.display= "none";
+    signin_form.style.display= "none";
+    signup_form.style.display= "none";
+    food_donation_form.style.display= "none";
+    food_require_form.style.display= "none";
+     
+
+
+     document.getElementById("our-partner").style.color="grey";
+     document.getElementById("main").style.color="rgb(40,144,255)";
+     document.getElementById("about-page").style.color="grey";
+     document.getElementById("donate-page").style.color="grey";
+     document.getElementById("contact-page").style.color="grey";
+     document.getElementById("login-page").style.color="grey";
+}
+
+
+function aboutus(){
+    
+    foot.style.display="block";
+    mainpage1.style.display = "none";
+    mainpage2.style.display = "none";
+    about1.style.display = "block";
+    about2.style.display = "block";
+    feedback_form.style.display= "none";
+    our_partner.style.display= "none";
+    signin_form.style.display= "none";
+    signup_form.style.display= "none";
+    food_donation_form.style.display= "none";
+    food_require_form.style.display= "none";
+     
+
+
+     document.getElementById("our-partner").style.color="grey";
+     document.getElementById("main").style.color="grey";
+     document.getElementById("about-page").style.color="rgb(40,144,255)";
+     document.getElementById("donate-page").style.color="grey";
+     document.getElementById("contact-page").style.color="grey";
+     document.getElementById("login-page").style.color="grey";
+}
+
+
+
+function partners(){
+    
+     foot.style.display="block";
+     mainpage1.style.display = "none";
+     mainpage2.style.display = "none";
+     about1.style.display = "none";
+     about2.style.display = "none";
+     feedback_form.style.display= "none";
+     our_partner.style.display= "block";
+     signin_form.style.display= "none";
+     signup_form.style.display= "none";
+     food_donation_form.style.display= "none";
+     food_require_form.style.display= "none";
+      
+
+
+      document.getElementById("our-partner").style.color="rgb(40,144,255)";
+      document.getElementById("main").style.color="grey";
+      document.getElementById("about-page").style.color="grey";
+      document.getElementById("donate-page").style.color="grey";
+      document.getElementById("contact-page").style.color="grey";
+      document.getElementById("login-page").style.color="grey";
+ }
+
+ function donate(){
+    
+    foot.style.display="block";
+    mainpage1.style.display = "none";
+    mainpage2.style.display = "none";
+    about1.style.display = "none";
+    about2.style.display = "none";
+    feedback_form.style.display= "none";
+    our_partner.style.display= "none";
+    signin_form.style.display= "none";
+    signup_form.style.display= "none";
+    food_donation_form.style.display= "block";
+    food_require_form.style.display= "block";
+     
+
+
+     document.getElementById("our-partner").style.color="grey";
+     document.getElementById("main").style.color="grey";
+     document.getElementById("about-page").style.color="grey";
+     document.getElementById("donate-page").style.color="rgb(40,144,255)";
+     document.getElementById("contact-page").style.color="grey";
+     document.getElementById("login-page").style.color="grey";
+}
+
+
+function contacts(){
+    
+    foot.style.display="block";
+    mainpage1.style.display = "none";
+    mainpage2.style.display = "none";
+    about1.style.display = "none";
+    about2.style.display = "none";
+    feedback_form.style.display= "block";
+    our_partner.style.display= "none";
+    signin_form.style.display= "none";
+    signup_form.style.display= "none";
+    food_donation_form.style.display= "none";
+    food_require_form.style.display= "none";
+     
+
+
+     document.getElementById("our-partner").style.color="grey";
+     document.getElementById("main").style.color="grey";
+     document.getElementById("about-page").style.color="grey";
+     document.getElementById("donate-page").style.color="grey";
+     document.getElementById("contact-page").style.color="rgb(40,144,255)";
+     document.getElementById("login-page").style.color="grey";
+}
+
+
+
+
+ function login(){
+    
+    foot.style.display="block";
+    mainpage1.style.display = "none";
+    mainpage2.style.display = "none";
+    about1.style.display = "none";
+    about2.style.display = "none";
+    feedback_form.style.display= "none";
+    our_partner.style.display= "none";
+    signin_form.style.display= "block";
+    signup_form.style.display= "none";
+    food_donation_form.style.display= "none";
+    food_require_form.style.display= "none";
+     
+
+
+     document.getElementById("our-partner").style.color="grey";
+     document.getElementById("main").style.color="grey";
+     document.getElementById("about-page").style.color="grey";
+     document.getElementById("donate-page").style.color="grey";
+     document.getElementById("contact-page").style.color="grey";
+     document.getElementById("login-page").style.color="rgb(40,144,255)";
+}
+
+
+
+function signup(){
+    
+     foot.style.display="block";
+     mainpage1.style.display = "none";
+     mainpage2.style.display = "none";
+     about1.style.display = "none";
+     about2.style.display = "none";
+     feedback_form.style.display= "none";
+     our_partner.style.display= "none";
+     signin_form.style.display= "none";
+     signup_form.style.display= "block";
+     food_donation_form.style.display= "none";
+     food_require_form.style.display= "none";
+      
+ 
+ 
+      document.getElementById("our-partner").style.color="grey";
+      document.getElementById("main").style.color="grey";
+      document.getElementById("about-page").style.color="grey";
+      document.getElementById("donate-page").style.color="grey";
+      document.getElementById("contact-page").style.color="grey";
+      document.getElementById("login-page").style.color="grey";
+      document.getElementById("signup-page").style.color="rgb(40,144,255)"
+ }
+
+
+ 
+function donate(){
+    
+     foot.style.display="block";
+     mainpage1.style.display = "none";
+     mainpage2.style.display = "none";
+     about1.style.display = "none";
+     about2.style.display = "none";
+     feedback_form.style.display= "none";
+     our_partner.style.display= "none";
+     signin_form.style.display= "none";
+     signup_form.style.display= "none";
+     food_donation_form.style.display= "block";
+     food_require_form.style.display= "none";
+      
+ 
+ 
+      document.getElementById("our-partner").style.color="grey";
+      document.getElementById("main").style.color="grey";
+      document.getElementById("about-page").style.color="grey";
+      document.getElementById("donate-page").style.color="grey";
+      document.getElementById("contact-page").style.color="grey";
+      document.getElementById("login-page").style.color="grey";
+      document.getElementById("signup-page").style.color="grey";
+ }
+
+
+ 
+function enquire(){
+    
+     foot.style.display="block";
+     mainpage1.style.display = "none";
+     mainpage2.style.display = "none";
+     about1.style.display = "none";
+     about2.style.display = "none";
+     feedback_form.style.display= "none";
+     our_partner.style.display= "none";
+     signin_form.style.display= "none";
+     signup_form.style.display= "none";
+     food_donation_form.style.display= "none";
+     food_require_form.style.display= "block";
+      
+ 
+ 
+      document.getElementById("our-partner").style.color="grey";
+      document.getElementById("main").style.color="grey";
+      document.getElementById("about-page").style.color="grey";
+      document.getElementById("donate-page").style.color="grey";
+      document.getElementById("contact-page").style.color="grey";
+      document.getElementById("login-page").style.color="grey";
+      document.getElementById("signup-page").style.color="grey";
+ }
+
